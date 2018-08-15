e3c0ec3f8725ce5efe4ebec2d9512cf049bb15f3
José Henrique Sabino <josesabino@outlook.com>
2018-08-15 00:57:19 -0300

    Primeiro commit
    Primeiro commit

---
+++ App17/App17/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Plugin.Share;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App17
+{
+	public partial class MainPage : ContentPage
+	{
+        List<Models1> ListaProdutos = new List<Models1>();
+        MainViewModel _vm;
+
+        public MainPage()
+		{
+			InitializeComponent();
+
+            ListaProdutos.Add(new Models1 { Nome = "Home", Imagem = "twotone_receipt_black_24dp.png" });
+            ListaProdutos.Add(new Models1 { Nome = "Dados Cadastrais", Imagem = "twotone_receipt_black_24dp.png" });
+            ListaProdutos.Add(new Models1 { Nome = "teste", Imagem = "twotone_receipt_black_24dp.png" });
+            ListaProdutos.Add(new Models1 { Nome = "teste", Imagem = "twotone_receipt_black_24dp.png" });
+            ListaProdutos.Add(new Models1 { Nome = "Pagamento", Imagem = "twotone_receipt_black_24dp.png" });
+            ListaProdutos.Add(new Models1 { Nome = "Cartão", Imagem = "twotone_receipt_black_24dp.png" });
+            ListaProdutos.Add(new Models1 { Nome = "Teste", Imagem = "twotone_receipt_black_24dp.png" });
+            Lista.ItemsSource = ListaProdutos;
+
+            BindingContext = _vm = new MainViewModel();
+        }
+
+        void Handle_PositionSelected(object sender, CarouselView.FormsPlugin.Abstractions.PositionSelectedEventArgs e)
+        {
+            Debug.WriteLine("Position " + e.NewValue + " selected.");
+        }
+
+        void Handle_Scrolled(object sender, CarouselView.FormsPlugin.Abstractions.ScrolledEventArgs e)
+        {
+            Debug.WriteLine("Scrolled to " + e.NewValue + " percent.");
+            Debug.WriteLine("Direction = " + e.Direction);
+           
+        }
+    }
+}
---
+++ App17/App17/Models1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App17
+{
+   public class Models1
+   {
+        public string Nome { get; set; }
+        public string Imagem { get; set; }
+   }
+}
